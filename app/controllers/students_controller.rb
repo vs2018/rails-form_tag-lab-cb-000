1d27d9ca2c5247a1450d0f3e8e5ecb4a80bd532c
Vishal Sharma <vsh1819@gmail.com>
2018-07-16 09:56:42 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,14 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
-
+  
+  def new
+    render "new"
+  end
+  
+  def create
+  @student =  Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  render "show"
+end
+  
 end
