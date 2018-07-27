14129157561e6a2f9e81f629297b3318c0c03398
Vishal Sharma <vsh1819@gmail.com>
2018-07-27 02:50:58 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,8 +13,9 @@ class StudentsController < ApplicationController
   end
   
   def create
-  @student =  Student.create(first_name: params[:first_name], last_name: params[:last_name])
-  render "show"
+      session[:form_params] = params.inspect
+
+  redirect_to new_student_path
 end
   
 end
